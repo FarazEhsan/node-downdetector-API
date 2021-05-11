6ffb1002b57a7c6fe792ce236228292934062556
Faraz Ehsan <faraz.ehsan@iengineering.com>
2021-05-11 18:38:27 +0500

    restful API: Routing Requests

--- api/index.js
+++ api/index.js
@@ -45,15 +45,43 @@ server= http.createServer((req,res)=>{
         //bind to the 'end' event which will tell us that the stream has ended
     req.on('end', ()=>{
         buffer+=decoder.end();
-    
-        //Send the response since the stream has ended 
-        res.end('Hello World\n');
 
-            //Log the requst  path 
-            console.log(//'Request was received on path: ', pathName, 
-                        // ', Request method was: ', method, ', Query string parameters: ',queryStringObj,
-                        // ', Request headers are: ',headers,
-                        ', Request recevced with this payload: ',buffer);
+        //select a chosen handler based on path name
+        console.log('Pathname is: ', pathName);
+        let chosenHandler= typeof(router[pathName])!=='undefined'?router[pathName]:handlers.notFound;
+
+        //create data to be sent to chosen handler and handle data callback 
+        let data ={
+            'pathName':pathName,
+            'method':method,
+            'headers':headers,
+            'payload':buffer
+        };
+
+        //call the chosen handler and send above data. Receive the data from callback of chosen handler
+        chosenHandler(data,(statusCode,payload)=>{
+            //get the status code called back by the handler or use default:200
+            statusCode=typeof(statusCode)=='number'?statusCode:200;
+
+            //get the payload called back by the handler or set default :{}
+            payload=typeof(payload)=='object'?payload:{};
+
+            //stringyfy the payload to return json string
+            const payloadString= JSON.stringify(payload);
+
+            //write the statuscode to res
+            res.writeHead(statusCode);
+            //Send the response since the stream has ended 
+            res.end(payloadString);
+            console.log('Returning this response: ', statusCode, payloadString);
+
+        });
+
+                    //Log the requst  path 
+                    //console.log(//'Request was received on path: ', pathName, 
+                    // ', Request method was: ', method, ', Query string parameters: ',queryStringObj,
+                    // ', Request headers are: ',headers,
+                   // ', Request recevced with this payload: ',buffer);
 
     })
 
@@ -67,3 +95,16 @@ server= http.createServer((req,res)=>{
 
 //Server will listen on port 3001
 server.listen(3001, ()=>{console.log('Server is running on port:3001')})
+
+//Define hadlers for route requests
+let handlers= {};
+handlers.sample= (data,callback)=>{
+    callback(408, {'name':'sample handler'});
+}
+handlers.notFound = (data,callback)=>{
+    callback(404);
+}
+//define handlers for all the paths
+const router = {
+    '/sample':handlers.sample
+}
