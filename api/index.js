d1d57b0c05da88dea57dbe91a61e83e4cce43681
Faraz Ehsan <faraz.ehsan@iengineering.com>
2021-05-11 15:53:31 +0500

    Building a restful API: Parsing payloads

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@
 
 const http=require('http')
 const url=require('url')
+const StringDecoder= require('string_decoder').StringDecoder; 
 
 
 //Create a server
@@ -19,18 +20,48 @@ server= http.createServer((req,res)=>{
 
     //Get the method i.e. GET/POST etc
     const method= req.method.toLowerCase();
+
+    //Get headers
+    const headers= req.headers;
     //Get the path
     const pathName= parsedUrl.pathname;
 
     //Get the queryString, cant use the parsedUrl.query since its depreciatedS
     const queryStringObj= parsedUrl.searchParams;
 
-    //Send the response 
-    res.end('Hello World')
+    //Get the payload stream in the buffer variable
+    let buffer=''
 
-    //Log the requst  path 
-    console.log('Request was received on path: ', pathName, 
-                ', Request method was: ', method, ', Query string parameters: ',queryStringObj);
+    //Create a string decoder to decode stream as string utf-8
+    let decoder= new StringDecoder('utf-8');
+
+    //bind to the 'data' event of req to get stream events that are being emitted
+    req.on('data', (data)=>{
+
+        //read the stream in buffer var using string decoder
+        buffer+=decoder.write(data);
+    })
+
+        //bind to the 'end' event which will tell us that the stream has ended
+    req.on('end', ()=>{
+        buffer+=decoder.end();
+    
+        //Send the response since the stream has ended 
+        res.end('Hello World\n');
+
+            //Log the requst  path 
+            console.log(//'Request was received on path: ', pathName, 
+                        // ', Request method was: ', method, ', Query string parameters: ',queryStringObj,
+                        // ', Request headers are: ',headers,
+                        ', Request recevced with this payload: ',buffer);
+
+    })
+
+
+
+
+
+
 })
 
 
